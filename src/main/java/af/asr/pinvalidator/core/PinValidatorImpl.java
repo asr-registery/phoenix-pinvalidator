a7bf4fd43059cff4dd4ede977251d08754af5a32
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-12 11:39:14 +0430

    Exception Handling and Generic Exceptions

--- src/main/java/af/asr/pinvalidator/core/PinValidatorImpl.java
+++ src/main/java/af/asr/pinvalidator/core/PinValidatorImpl.java
@@ -1,4 +1,73 @@
 package af.asr.pinvalidator.core;
 
-public class PinValidatorImpl {
+import java.util.regex.Pattern;
+
+import javax.annotation.PostConstruct;
+
+import af.asr.pinvalidator.exception.pinvalidator.exception.InvalidPinException;
+import af.asr.pinvalidator.exception.pinvalidator.spi.PinValidator;
+import af.asr.pinvalidator.util.PinExceptionConstant;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+/**
+ * This class for validate the given pin in the String format
+ *
+ */
+@Component
+public class PinValidatorImpl implements PinValidator<String> {
+    /**
+     * This variable to hold Pin length
+     *
+     */
+    @Value("${phoenix.kernel.pin.length}")
+    private int pinLength;
+    /**
+     * Validate the pin, It should not contain any alphanumeric characters
+     */
+    private String numaricRegEx;
+
+    /**
+     * Method to prepare regular expressions for checking pin has only digits.
+     */
+    @PostConstruct
+    private void uinValidatorImplnumaricRegEx() {
+        numaricRegEx = "\\d{" + pinLength + "}";
+    }
+
+    @Override
+    public boolean validatePin(String pin) {
+        /**
+         *
+         * Check Pin, It Shouldn't be Null or empty
+         *
+         */
+        if (StringUtils.isEmpty(pin)) {
+            throw new InvalidPinException(PinExceptionConstant.PIN_INVALID_NULL.getErrorCode(),
+                    PinExceptionConstant.PIN_INVALID_NULL.getErrorMessage());
+        }
+        /**
+         *
+         * Check the Length of the pin, It Should be specified number of digits
+         *
+         */
+
+        if (pin.length() != pinLength) {
+            throw new InvalidPinException(PinExceptionConstant.PIN_INVALID_LENGTH.getErrorCode(),
+                    PinExceptionConstant.PIN_INVALID_LENGTH.getErrorMessage() + pinLength);
+        }
+        /**
+         *
+         * Validate the pin, It should not contain any alphanumeric characters
+         *
+         */
+        if (!Pattern.matches(numaricRegEx, pin)) {
+            throw new InvalidPinException(PinExceptionConstant.PIN_INVALID_CHAR.getErrorCode(),
+                    PinExceptionConstant.PIN_INVALID_CHAR.getErrorMessage());
+        }
+
+        return true;
+    }
+
 }
