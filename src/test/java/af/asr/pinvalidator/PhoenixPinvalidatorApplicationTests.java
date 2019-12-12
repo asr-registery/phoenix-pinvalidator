3359b26e0fd046cf2fdd5e33b50d3a78b720d5a4
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-12 11:32:31 +0430

    init

---
+++ src/test/java/af/asr/pinvalidator/PhoenixPinvalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.pinvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixPinvalidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
